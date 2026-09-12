1bce6d33cf95d0e77317fbe296878946212deb2b
Kerenjustin286@gmail.com <kerenjustin286@gmail.com>
2026-09-12 10:35:05 +0700

    Updated Progress

---
+++ Assets/Script/EndingManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class EndingManager : MonoBehaviour
+{
+    public GameObject endingPanel;
+    public TMP_Text endingText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartEnding()
+    {
+        endingPanel.SetActive(true);
+        StartCoroutine(EndingSequence());
+    }
+
+    // Update is called once per frame
+    IEnumerator EndingSequence()
+    {
+        endingText.text = "YOU THOUGHT THEY WERE TWO.";
+        yield return new WaitForSeconds(3f);
+
+        endingText.text = "BUT THERE WAS ONLY ONE SOUL.";
+        yield return new WaitForSeconds(3f);
+
+        endingText.text = "THE END";
+    }
+}
