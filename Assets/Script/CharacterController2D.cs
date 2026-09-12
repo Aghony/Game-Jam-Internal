d2df9ae07ce97b0f642372ebc25e4d42715b788f
Kerenjustin286@gmail.com <kerenjustin286@gmail.com>
2026-09-12 17:22:35 +0700

    Updated Progress

--- Assets/Script/CharacterController2D.cs
+++ Assets/Script/CharacterController2D.cs
@@ -4,16 +4,23 @@ public class CharacterController2D : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 12f;
+    public Vector3 cameraOffset;
+    public float normalGravity = 3f;
+    public float fallGravity = 5f;
 
     private Rigidbody2D rb;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
     private bool isGrounded;
+
     public Transform cameraTransform;
     public float cameraSmoothSpeed = 5f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -36,25 +43,54 @@ public class CharacterController2D : MonoBehaviour
         SetAnimation(horizontal);
     }
 
-    void LateUpdate()
+    void FixedUpdate()
     {
-        if(cameraTransform == null ) return;
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.gravityScale = fallGravity;
+        }
+        else
+        {
+            rb.gravityScale = normalGravity;
+        }
+    }
 
-        Vector3 targetPosition = new Vector3(
-            transform.position.x,
-            transform.position.y,
-            cameraTransform.position.z
-        );
+    void LateUpdate()
+{
+    if (cameraTransform == null)
+        return;
 
-        cameraTransform.position = Vector3.Lerp(
+    Vector3 targetPosition = transform.position + cameraOffset;
+
+    targetPosition.z = cameraTransform.position.z;
+
+    cameraTransform.position = Vector3.Lerp(
         cameraTransform.position,
         targetPosition,
         cameraSmoothSpeed * Time.deltaTime
-        );
-    }
+    );
+}
 
     void SetAnimation(float horizontal)
     {
+        // Sedang di udara
+        if (!isGrounded)
+        {
+            if (gameObject.name == "Soldier")
+                animator.Play("Soldier_Jump");
+            else if (gameObject.name == "Slime")
+                animator.Play("Slime_Jump");
+
+            return;
+        }
+
+        // Arah karakter
+        if (horizontal > 0)
+            spriteRenderer.flipX = false;
+        else if (horizontal < 0)
+            spriteRenderer.flipX = true;
+
+        // Sedang berjalan
         if (horizontal != 0)
         {
             if (gameObject.name == "Soldier")
@@ -62,12 +98,8 @@ public class CharacterController2D : MonoBehaviour
             else if (gameObject.name == "Slime")
                 animator.Play("Slime_Walk");
         }
-            
-            if (horizontal > 0)
-                GetComponent<SpriteRenderer>().flipX = false;
-            else if (horizontal < 0)
-                GetComponent<SpriteRenderer>().flipX = true;
-            else
+        // Diam
+        else
         {
             if (gameObject.name == "Soldier")
                 animator.Play("Soldier_Idle");
@@ -91,4 +123,4 @@ public class CharacterController2D : MonoBehaviour
             isGrounded = false;
         }
     }
-}  
+}
